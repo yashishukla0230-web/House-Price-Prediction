6a85863641a93463ff1af613bdb3583ff60c7ac5
yashishukla0230-web <yashishukla0230@gmail.com>
2025-12-29 10:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,40 +9,40 @@ st.title('🏠House Price prediction using ML')
 
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
 
-data = fetch_california_housing()
+df = pd.read_csv('house_data.csv')
+X = df.iloc[:,:-3]
+y = df.iloc[:,-1]
 
-X = pd.DataFrame(data['data'],
-            columns = data['feature_names'])
-
-final_X = X.iloc[:,:-2]
+final_X = X
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
 
-st.sidebar.title('select house features: ')
-st.sidebar.image('https://cdn.dribble.com/userupload/20000742/file/original-aaf23458355a15
+st.sidebar.title('Select House features: ')
+st.sidebar.image('https://cdn.dribbble.com/userupload/20000742/file/original-aaf23458355a156d0cf85b8217a5065a.gif')
 all_value = []
 for i in final_X:
   min_value = final_X[i].min()
   max_value = final_X[i].max()
-  result = st.sidebar.slider(f'select {i} value',min_value.max_value)
+  result = st.sidebar.slider(f'Select {i} value',min_value,max_value)
   all_value.append(result)
 
-user_X = scaler.transform([all_values])
+
+user_X = scaler.transform([all_value])
 
 @st.cache_data
 def ml_model(X,y):
   model = RandomForestRegressor()
-  model.fit(scaled_X,y)
+  model.fit(X,y)
   return model
 
-model = ml_model(scaaled_X,y)
-House_price = model.predict(user_X)[0]
+model = ml_model(scaled_X,y)
+house_price = model.predict(user_X)[0]
 
-final_price = round(house_price = 100000,2)
+final_price = round(house_price * 100000,2)
 
-with st.spinner('predicting House Price'):
-   import time
-   time.sleep(2)
+with st.spinner('Predicting House Price'):
+  import time
+  time.sleep(2)
 
 st.success(f'Estimated House Price is: $ {final_price}')
-st.markdown('''**Design and Developed by: YASHI SHUKLA**''')
+st.markdown('''*Design and Developed by: Yashi Shukla*''')
